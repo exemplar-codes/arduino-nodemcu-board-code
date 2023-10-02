9fcc3251d8be5ca6f695dd96b9b9c3b81c63a5e3
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-10-03 00:40:00 +0530

    Add cors

--- nodemcu/POST_polling_relay_control_nodemcu/server-app/app.js
+++ nodemcu/POST_polling_relay_control_nodemcu/server-app/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 4000;
 
 let switchValues = [true, true, false, false];
